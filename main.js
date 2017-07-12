ae39219a5813ef8abbb98275a0617e46e0fba141
sanlouise <sandra.adamshallie@gmail.com>
2017-07-11 21:38:35 -0400

    Major refactor of all JS

--- main.js
+++ main.js
@@ -1,137 +1,58 @@
-// The Form Data
-let formData = [
-  // For demonstration purposes,
-  // the first form element has been added to the HTML file as a comment
-  // compare the input in the HTML file with the contents of this first object
-  {
-    "type": "text",
-    "label": "First Name",
-    "id": "user-first-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Last Name",
-    "id": "user-last-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "email",
-    "label": "Email Address",
-    "id": "user-email",
-    "icon": "fa-envelope",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Current Website URL",
-    "id": "user-website",
-    "icon": "fa-globe",
-    "options": []
-  },
-  {
-    "type": "select",
-    "label": "Select Language",
-    "id": "user-language",
-    "icon": "",
-    "options": [
-      {
-        "label": "English",
-        "value": "EN"
-      },
-      {
-        "label": "French",
-        "value": "FR"
-      },
-      {
-        "label": "Spanish",
-        "value": "SP"
-      },
-      {
-        "label": "Chinese",
-        "value": "CH"
-      },
-      {
-        "label": "Japanese",
-        "value": "JP"
-      }
-    ]
-  },
-  {
-    "type": "textarea",
-    "label": "Your Comment",
-    "id": "user-comment",
-    "icon": "fa-comments",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Mobile Number",
-    "id": "user-mobile",
-    "icon": "fa-mobile-phone",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Home Number",
-    "id": "user-phone",
-    "icon": "fa-phone",
-    "options": []
-  }
-];
-
-// -------- Your Code Goes Below this Line --------
-for (let i = 0; i < formData.length; i++) {
-
-  // Set constants
-  const item = formData[i];
-  const { options, type, id, icon, label } = item;
-  const form = document.querySelector("#fields");
-  const input = document.createElement("input");
-  const select = document.createElement("select");
-  const textarea = document.createElement("textarea");
-  let option;
-
-  let createOption = (options) => {
-    form.appendChild(select);
-    option = document.createElement("option");
-    option.innerHTML = "Select language...";
-    option.setAttribute("value", "disabled");
-    select.appendChild(option);
-    for (let v = 0; v < options.length; v++) {
-      option = document.createElement("option");
-      option.innerHTML = options[v].label;
-      option.setAttribute("value", options[v].value);
-      select.appendChild(option);
+const setAttributesToElements = (elements, options, id, type, label) => {
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    if (type === 'select') {
+      element.setAttribute("options", options);
     }
+    element.setAttribute("id", id);
+    element.setAttribute("type", type);
+    element.setAttribute("placeholder", label);
   }
+};
 
-  // Set attributes for input fields
-  input.setAttribute("options", options);
-  input.setAttribute("id", id);
-  input.setAttribute("type", item);
-  input.setAttribute("placeholder", label);
+const createSelectOptions = ({ form, select, options }) => {
+  form.appendChild(select);
+  const firstOption = document.createElement("option");
+  firstOption.innerHTML = "Select language...";
+  firstOption.setAttribute("value", "disabled");
+  select.appendChild(firstOption);
+  for (let v = 0; v < options.length; v++) {
+    const option = document.createElement("option");
+    option.innerHTML = options[v].label;
+    option.setAttribute("value", options[v].value);
+    select.appendChild(option);
+  }
+};
 
-  // Set attributes for alternative input fields
-  select.setAttribute("placeholder", label);
-  textarea.setAttribute("placeholder", label);
+const createElements = (elements) => {
+  return elements.reduce((result, element) => {
+    result.push(document.createElement(element));
+    return result;
+  }, []);
+}
 
-  if (item.type == "textarea") {
+for (let i = 0; i < formData.length; i++) {
+  // Set constants
+  const { options, type, id, icon, label } = formData[i];
+  const form = document.querySelector("#fields");
+  // Destructuring at its finest
+  const [input, select, textarea] = createElements(["input", "select", "textarea"]);
+
+  // Set attributes for fields
+  const elements = [input, select, textarea];
+  setAttributesToElements(elements, options, id, type, label);
+
+  if (type === "textarea") {
     input.setAttribute("rows", 5)
+    form.appendChild(textarea);
   }
 
-  if (item.type != "select" && item.type != "textarea") {
+  if (type !== "select" && type !== "textarea") {
     form.appendChild(input);
   }
 
   // Create options dropdown
-  if (item.type === "select") {
-    createOption(options);
-  }
-
-  if (item.type === "textarea") {
-    form.appendChild(textarea);
+  if (type === "select") {
+    createSelectOptions({ options, form, select });
   }
 }
